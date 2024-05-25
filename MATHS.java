bf5eccd822a5350f444dd6f06f0a7ba85fac9037
neelima-rahman <158712381+neelima-rahman@users.noreply.github.com>
2024-05-25 17:26:36 +0600

    Add files via upload

---
+++ MATHS.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MATHS {
+
+		public static void main(String[] args) {
+			 Scanner scanner = new Scanner(System.in);
+
+			 
+			    System.out.print("A = ");
+		        
+		        int A = scanner.nextInt();
+		        System.out.print("B = ");
+		        int B = scanner.nextInt();
+		        
+		        System.out.print("C = ");
+		        int C = scanner.nextInt();
+		        
+			   
+		        int SUM = A + B + C;
+		        System.out.println("SUM = " + SUM);
+
+		        
+	}
+}
+
+
